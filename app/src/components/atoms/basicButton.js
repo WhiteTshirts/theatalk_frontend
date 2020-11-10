41ce9270aef5a20a24042593678f1fa0573ac2d8
rurito0125 <rurito.blog@gmail.com>
2020-11-10 20:48:34 +0900

    refactor: rename submitButton to basicButton

---
+++ app/src/components/atoms/basicButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+/**
+ * クリックされると呼び出し元のformのsubmitを発火させるボタン
+ * @param {string} props.msg ボタンのメッセージ名
+ * @param {string} props.backgroundColor ボタンの背景色(CSS)
+ * @param {boolean} props.onForm formに紐付いたsubmitButtonかどうか
+ * @param {function} props.handleClick クリックされた時に呼び出す関数
+ */
+const BasicButton = (props) => {
+  const useStyles = makeStyles(() => ({
+    button: {
+      color: 'white',
+      backgroundColor: props.backgroundColor,
+    },
+  }));
+
+  const styles = useStyles();
+
+  return (
+    <>
+      { /* formに紐付いている場合     */
+        props.onForm &&
+        <Button className={styles.button} type="submit" variant="contained">
+          {props.msg}
+        </Button>
+      }
+
+      { /* formに紐付いていない場合  */
+        !props.onForm &&
+        <Button className={styles.button} onClick={props.handleClick} variant="contained">
+          {props.msg}
+          {props.msg}
+        </Button>
+      }
+    </>
+  )
+};
+
+export default BasicButton;
