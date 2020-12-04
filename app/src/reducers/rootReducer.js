af5e3d5fa11bcc81999f30ae186dcd958d7d0631
ruritoBlogger <rurito.blog@gmail.com>
2020-12-04 22:41:01 +0900

    feat: user機能を移植

--- app/src/reducers/rootReducer.js
+++ app/src/reducers/rootReducer.js
@@ -4,6 +4,8 @@ import room from './roomReducer';
 import auth from './authReducer';
 import createRoom from './createRoomReducer';
 import tags from './tagReducer';
+import user from './userReducer';
+import users from './usersReducer';
 import userTags from './userTagReducer';
 import chat from './chatReducer';
 import searchedRoom from './searchedRoomsReducer';
@@ -14,6 +16,8 @@ const rootReducer = combineReducers({
   auth,
   createRoom,
   tags,
+  user,
+  users,
   userTags,
   chat,
   searchedRoom,
